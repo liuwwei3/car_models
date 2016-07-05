8738c1590d80eeb859632de8076bf496869ad6a5
liuweiwei02 <liuweiwei02@baidu.com>
2016-07-06 00:54:36 +0800

    add a plugin for the car

--- src/car/CarModel.cc
+++ src/car/CarModel.cc
@@ -14,27 +14,27 @@ class CarPlugin : public ModelPlugin {
 
 public:
     void Load( physics::ModelPtr _model, sdf::ElementPtr _element){
-        physics::LinkPtr applebody = _model->GetChildLink("applebody");
-        physics::InertialPtr _inertial(new physics::Inertial(1.0));
-        applebody->SetInertial(_inertial);
+        this->frwheel = _model->GetChildLink("FRwheel");
+        this->flwheel = _model->GetChildLink("FLwheel");
         
         this->model = _model;
         this->upconn = event::Events::ConnectWorldUpdateBegin(
-            boost::bind( &ApplePlugin::onUpdate, this, _1));
+            boost::bind( &CarPlugin::onUpdate, this, _1));
     }
 
     void onUpdate(const common::UpdateInfo _info) {
         // update rate: perhaps 100hz
-        //this->model->SetLinearVel(math::Vector3(0.0, 0, 0.05));
-        this->model->GetChildLink("applebody")->AddForceAtRelativePosition( \
-            math::Vector3(0.0, 0, 0.0), math::Vector3(0.1, 0, 0.5));
+        this->frwheel->AddRelativeTorque(math::Vector3(0,1,0));
+        this->flwheel->AddRelativeTorque(math::Vector3(0,1,0));
     }
 
 private:
     physics::ModelPtr model;
     event::ConnectionPtr upconn;
+    physics::LinkPtr frwheel;
+    physics::LinkPtr flwheel;
 };
 
-GZ_REGISTER_MODEL_PLUGIN(ApplePlugin);
+GZ_REGISTER_MODEL_PLUGIN(CarPlugin);
 
 }
